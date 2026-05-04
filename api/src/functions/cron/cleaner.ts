f765ec44eb340035d10b2ebe8e5b0ff55de663e9
gandolphinnn <luca.gandolfi@naar.com>
2026-05-04 16:59:29 +0200

    Cleaner function removed debugging code

--- api/src/functions/cron/cleaner.ts
+++ api/src/functions/cron/cleaner.ts
@@ -2,10 +2,8 @@ import { InnerTimeFunction, PlayfabCtx, registerTimeFunction } from '../../';
 
 const cleanerRoomsInner: InnerTimeFunction = async (_timer, notifier) => {
 	const rooms = await PlayfabCtx.rooms.list();
-	console.log('@232171:', rooms);
 	const twentyMinutesAgo = new Date(Date.now() - 20 * 60 * 1000);
 	const inactiveRooms = rooms.filter(r => r.lastUpdate < twentyMinutesAgo);
-	console.log('@331067:', inactiveRooms);
 	if (!inactiveRooms.length) return;
 
 	for (const room of inactiveRooms) {
@@ -19,5 +17,5 @@ const cleanerPlayersInner: InnerTimeFunction = async (_timer, _context) => {
 	//TODO: Remove all the guest players that have been inactive for a while
 };
 
-registerTimeFunction('cleaner_rooms', '0 */20 * * * *', true, cleanerRoomsInner);
+registerTimeFunction('cleaner_rooms', '0 */20 * * * *', false, cleanerRoomsInner);
 //registerTimeFunction('cleaner_players', '0 */20 * * * *', true, cleanerPlayersInner);
